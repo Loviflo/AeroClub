aa766a0f9b6becb6c6ed305db44351de14b5b434
Vivian <vivian.fr@free.fr>
2021-07-25 18:17:56 +0200

    Month Calendar

--- index.php
+++ index.php
@@ -1,12 +1,51 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <?php include 'utils/head.php'; ?>
     <title>Accueil</title>
+    <link rel="stylesheet" href="CSS/calendar.css">
+    <?php
+    ini_set("display_errors", 1);
+    ?>
 </head>
+
 <body>
     <?php include 'utils/header.php'; ?>
-    <h1>Hello</h1>
+
+    <?php
+    require 'src/Date/Month.php';
+    $month = new Month($_GET["month"] ?? null, $_GET["year"] ?? null);
+    $start = $month->getFirstDay()->modify('last monday');
+    ?>
+
+    <div class="d-flex flex-row align-items-center justify-content-between mx-sm-3">
+        <h1><?= $month->toString(); ?></h1>
+        <div>
+            <a href="?month=<?= $month->previousMonth()->month; ?>&year=<?= $month->previousMonth()->year; ?>" class="btn btn-primary">&lt;</a>
+            <a href="?month=<?= $month->nextMonth()->month; ?>&year=<?= $month->nextMonth()->year; ?>" class="btn btn-primary">&gt;</a>
+        </div>
+    </div>
+
+
+    <table class="table calendar__table calendar__table--<?= $month->getWeeks(); ?>weeks">
+        <?php for ($i = 0; $i < $month->getWeeks(); $i++) : ?>
+            <tr>
+                <?php foreach ($month->days as $k => $day) :
+                    $date = (clone $start)->modify("+" . $k + $i * 7 . " days");
+                ?>
+                    <td class="<?= $month->withinMonth($date) ? '' : 'calendar__othermonth'; ?>">
+                        <?php if ($i === 0) : ?><div class="calendar__weekday"><?= $day; ?></div><?php endif; ?>
+                        <div class="calendar__day"><?= $date->format('d'); ?></div>
+                    </td>
+                <?php endforeach; ?>
+            </tr>
+        <?php endfor; ?>
+
+    </table>
+
+
     <?php include 'utils/footer.php'; ?>
 </body>
+
 </html>
---
+++ src/Date/Month.php
@@ -0,0 +1,111 @@
+<?php
+
+
+class Month
+{
+
+    public $days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+    private $months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+    public $month;
+    public $year;
+
+    /**
+     * Month constructor
+     * @param int $month Le mois compris entre 1 et 12
+     * @param int $year L'année
+     * @throws Exception
+     */
+
+    public function __construct(?int $month = null, ?int $year = null)
+    {
+        if ($month === null || $month < 1 || $month > 12) {
+            $month = intval(date('m'));
+        }
+
+        if ($year === null) {
+            $year = intval(date('Y'));
+        }
+        
+        $this->month = $month;
+        $this->year = $year;
+    }
+
+    /**
+     * Retourne le premier jour du mois
+     */
+
+    public function getFirstDay(): DateTime
+    {
+        return new \DateTime("{$this->year}-{$this->month}-01");
+    }
+
+    /**
+     * Retourne le mois en toute lettre
+     * @return string
+     */
+
+    public function toString(): string
+    {
+        return $this->months[$this->month - 1] . ' ' . $this->year;
+    }
+
+    /**
+     * Retourne le nombre de semaine dans le mois
+     * @return int
+     */
+
+    public function getWeeks(): int
+    {
+        $start = $this->getFirstDay();
+        $end = (clone $start)->modify('+1 month -1 day');
+        $weeks = intval($end->format('W')) - intval($start->format('W')) + 1;
+        if ($weeks < 0) {
+            $weeks = intval($end->format('W'));
+        }
+        return $weeks;
+    }
+
+    /**
+     * Est-ce que le jour est dans le mois en cours
+     * @param DateTime $date
+     * @return bool
+     */
+
+    public function withinMonth(DateTime $date): bool
+    {
+        return $this->getFirstDay()->format('Y-m') === $date->format('Y-m');
+    }
+
+    /**
+     * Renvoie le mois suivant
+     * @return Month
+     */
+
+    public function nextMonth(): Month
+    {
+        $month = $this->month + 1;
+        $year = $this->year;
+        if ($month > 12) {
+            $month = 1;
+            $year += 1;
+        }
+        return new Month($month, $year);
+    }
+
+    /**
+     * Renvoie le mois précédent
+     * @return Month
+     */
+
+    public function previousMonth(): Month
+    {
+        $month = $this->month - 1;
+        $year = $this->year;
+        if ($month < 1) {
+            $month = 12;
+            $year -= 1;
+        }
+        return new Month($month, $year);
+    }
+}
