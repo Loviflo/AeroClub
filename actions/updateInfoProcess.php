2f0adf69fd2457ddb879dbb9897099c273ac8764
Vivian <vivian.fr@free.fr>
2021-08-27 23:12:44 +0200

    Fix Update Info Process

--- actions/updateInfoProcess.php
+++ actions/updateInfoProcess.php
@@ -6,14 +6,14 @@ session_start();
 
 $db = getDatabaseConnection();
 
-if (isset($_POST['firstname'])){
+if (isset($_POST['firstname']) && $_POST['firstname'] != null){
     $firstname = htmlspecialchars($_POST['firstname']);
     $q = 'UPDATE members SET firstname = ? WHERE id = ?';
     $req = $db->prepare($q);
     $req->execute([$firstname, $_SESSION['user']['id']]);
 }
 
-if (isset($_POST['lastname'])){
+if (isset($_POST['lastname']) && $_POST['lastname'] != null){
     $lastname = htmlspecialchars($_POST['lastname']);
     $q = 'UPDATE members SET lastname = ? WHERE id = ?';
     $req = $db->prepare($q);
@@ -28,13 +28,13 @@ if (isset($_POST['lastname'])){
 }*/
 
 if (isset($_POST['license'])){
-    $license = $_POST['lastname'];
+    $license = $_POST['license'];
     $q = 'UPDATE members SET additionalCost = ? WHERE id = ?';
     $req = $db->prepare($q);
     $req->execute([$license, $_SESSION['user']['id']]);
 }
 
-if (isset($_POST['password']) && isset($_POST['passwordConf'])){
+if ((isset($_POST['password']) && isset($_POST['passwordConf'])) && $_POST['password'] != null){
     if ($_POST['password'] === $_POST['passwordConf']){
         $password = hash('sha512', $_POST['password']);
         $q = 'UPDATE members SET password = ? WHERE id = ?';
